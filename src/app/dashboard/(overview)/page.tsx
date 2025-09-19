e7a98e3b7dc607eb9dde746bab864197c18e558f
Maksym Lysenko <korgara93@gmail.com>
2025-09-19 13:18:24 +0300

    more

---
+++ src/app/dashboard/(overview)/page.tsx
@@ -0,0 +1,29 @@
+import { Suspense } from "react";
+import { lusitana } from "../../ui/fonts";
+import CardWrapper, { Card } from "../ui/cards";
+import LatestInvoices from "../ui/latest-invoices";
+import RevenueChart from "../ui/revenue-chart";
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
+
+export default async function Page() {
+	return (
+		<main>
+			<h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+				Dashboard
+			</h1>
+			<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+				<Suspense fallback={<CardsSkeleton/>}>
+					<CardWrapper/>
+				</Suspense>
+			</div>
+			<div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+				<Suspense fallback={<RevenueChartSkeleton/>}>
+					<RevenueChart />
+				</Suspense>
+				<Suspense fallback={<LatestInvoicesSkeleton/>}>
+					<LatestInvoices />
+				</Suspense>
+			</div>
+		</main>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import { lusitana } from "./ui/fonts";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+    <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12 flex-col">
       <h1 className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>Hello World</h1>
-      <Image
-        src="/jackson.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
+      <div className="flex-col">
+        <Link href="/dashboard">start</Link>
+        <Image
+          src="/jackson.png"
+          width={1000}
+          height={760}
+          className="hidden md:block"
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+      </div>
     </div>
   );
 }
