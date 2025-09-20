740a5d9a87a3899afc3a561534859a73c87cf048
Maksym Lysenko <korgara93@gmail.com>
2025-09-20 22:33:02 +0300

    error handling

--- src/app/dashboard/invoices/edit-form.tsx
+++ src/app/dashboard/invoices/edit-form.tsx
@@ -19,7 +19,7 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   return (
-    <form action={(a) => updateInvoice(invoice.id, a)}>
+    <form action={(a) => {updateInvoice(invoice.id, a)}}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
